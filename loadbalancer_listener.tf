f4a83657778da0ae1be67e598b90361fc1931155
Chris Sim <monkeymon@gmail.com>
2022-03-23 23:32:34 +0800

    remove unused code and files

--- loadbalancer_listener.tf
+++ loadbalancer_listener.tf
@@ -1,36 +1,7 @@
-# resource "aws_lb_listener" "redirect_http_https" {
-#   load_balancer_arn = data.aws_lb.wordpress.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type = "redirect"
-
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
-
 resource "aws_lb_listener_certificate" "app-ssl-cert" {
   listener_arn    = data.aws_lb_listener.https_listener.arn
   certificate_arn = data.aws_acm_certificate.issued.arn
 }
-
-# resource "aws_lb_listener" "default_listener_rule" {
-#   load_balancer_arn = data.aws_lb.wordpress.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = data.aws_acm_certificate.issued.arn
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb-https-tg.arn
-#   }
-# }
 
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.aws_lb_listener.https_listener.arn
