af1a7af2b7e914aef335c2fd36d911de253e2ad4
Chris Sim <monkeymon@users.noreply.github.com>
2022-04-15 02:38:13 +0800

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -87,3 +87,78 @@ resource "aws_ecr_repository_policy" "php-fpm-policy" {
   }
   EOF
 }
+
+resource "aws_ecr_lifecycle_policy" "php-fpm-lifecycle-policy" {
+  repository = aws_ecr_repository.php-fpm-container.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 5 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v", "V"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_ecr_lifecycle_policy" "nginx-lifecycle-policy" {
+  repository = aws_ecr_repository.nginx-container.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 5 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v", "V"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
